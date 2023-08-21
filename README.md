718733aa5128e22f4d3e55c0edc4fb93b770ce1c
Alan Boris <alanboris@gmail.com>
2023-08-21 02:07:32 -0400

    Initial release

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # balena-greengrass-v2
-Run an AWS Greengrass core device on balena.
+Create an "AWS IoT Thing" (a Greengrass core device) that runs on balenaOS. 
+
+## Getting started
+
+You'll need an AWS account and an AWS Identity and Access Management (IAM) user with administrator permissions (See [this section](https://docs.aws.amazon.com/greengrass/v2/developerguide/getting-started-prerequisites.html) for more details. 
+
+You'll also need a balena account (the first 10 devices are free and full-featured) by signing up [here](https://dashboard.balena-cloud.com/signup).
+
+Then, follow these steps:
+
+1. Create a new fleet in your balenaCloud dashboard, and add a new device to the fleet.
+
+2. Set the following [device variables](https://docs.balena.io/learn/manage/variables/) for your AWS IoT Thing in the balenaCloud dashboard (do this before pushing any code to your fleet!):
+
+- `AWS_REGION` such as `us-east-1` or whichever is used for your devices (usually seen in the upper right of your AWS dashboard)
+
+- `AWS_ACCESS_KEY_ID` and `AWS_SECRET_ACCESS_KEY` from an authorized AWS user on your account. See "Security Credentials" under your username to generate access keys.
+
+- `GG_THING_NAME` should be set to the name you want to give your AWS IoT Thing.
+
+- `GG_GROUP_NAME` is optional... don't assign any value to this variable to deploy your core device without assigning a AWS IoT thing group. Otherwise, enter a new or existing group name.
+
+3. Clone this repo to your development computer, and use the [balena CLI](https://docs.balena.io/reference/balena-cli/) to push this code to your balena fleet.
+
+## Setup
+
+Once you have pushed this code to your device, it will use the variables you have assigned above to:
+
+- Install Java (Corretto 11) on the device
+
+- Download and run the AWS IoT Greengrass installer on the device (in a container) that provisions the Greengrass core device as an AWS IoT thing with a device certificate and default permissions.
+
+When the installer completes, you can find your device in the list of Greengrass core devices on the AWS Core devices page. You can then use the AWS IoT Greengrass consoleto deploy a component (such as a Lambda function) to the target device.
---
+++ start.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+
+FILE=/user-data/installed.txt
+
+if [ -f "$FILE" ]; then
+    echo "Device is already provisioned."
+else 
+    echo "Device is not provisioned... starting provisioning steps."
+    # check for required env var values
+    requirements=True
+    if [[ -z $AWS_REGION ]]; then
+        echo "You must set AWS_REGION before this device can be provisioned!"
+        requirements=False
+    else
+        echo "Found AWS_REGION value $AWS_REGION."
+    fi
+    
+    if [[ -z $AWS_ACCESS_KEY_ID ]]; then
+        echo "You must set AWS_ACCESS_KEY_ID before this device can be provisioned!"
+        requirements=False
+    else
+        echo "Found AWS_ACCESS_KEY_ID value."
+    fi
+ 
+    if [[ -z $AWS_SECRET_ACCESS_KEY ]]; then
+        echo "You must set AWS_SECRET_ACCESS_KEY before this device can be provisioned!"
+        requirements=False
+    else
+        echo "Found AWS_SECRET_ACCESS_KEY value."
+    fi
+
+    if [[ -z $GG_THING_NAME ]]; then
+        echo "You must set GG_THING_NAME before this device can be provisioned!"
+    requirements=False
+    else
+        echo "Found GG_THING_NAME value $GG_THING_NAME."
+    fi
+    
+    # if not provisioned, and required env vars available, run the installer
+
+    # (starting in /usr/src)
+
+    if [ requirements = "True" ]
+    then
+        echo "STARTING INSTALL..."
+    
+        if [[ -z $GG_GROUP_NAME ]]; then
+            echo "No GG_GROUP_NAME provided; not adding thing to an existing group."
+            java -Droot="/greengrass/v2" -Dlog.store=FILE -jar ./GreengrassInstaller/lib/Greengrass.jar --aws-region "$AWS_REGION" --thing-name "$GG_THING_NAME" --component-default-user ggc_user:ggc_group --provision true --setup-system-service false --deploy-dev-tools true
+        else
+            echo "Found GG_GROUP_NAME value; adding thing to $GG_GROUP_NAME group."
+            java -Droot="/greengrass/v2" -Dlog.store=FILE -jar ./GreengrassInstaller/lib/Greengrass.jar --aws-region "$AWS_REGION" --thing-name "$GG_THING_NAME" --thing-group-name "$GG_GROUP_NAME" --component-default-user ggc_user:ggc_group --provision true --setup-system-service false --deploy-dev-tools true
+        fi
+        
+        # Set flag to let us know this was provisioned
+        touch "$FILE"
+    
+    else
+        echo "NOT installing software - requirements not met - see above..."
+        
+    fi        
+
+fi
+
+# Start AWS IoT Core Software and nucleus
+
+if [ -f "$FILE" ]; then
+    echo "STARTING AWS IoT Core and Nucleus."
+
+    /bin/sh /greengrass/v2/alts/current/distro/bin/loader
+else
+    echo "Software is not installed..."
+fi
+
+
+
+
+
+
